1c04204e0adf6fa69c5a07db1dfc0c3a6d7e2fd1
KARNA-2006 <shruti.kittu06@gmail.com>
2025-10-23 02:39:15 +0530

    intialized sample listing on mongodb atlas

--- init/index.js
+++ init/index.js
@@ -1,8 +1,13 @@
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config({path: '../.env'});
+}
+
 const mongoose=require("mongoose");
 const initData=require("./data.js");
 const Listing=require("../models/listing.js");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl= process.env.ALTASDB_URL
+console.log(dbUrl)
 main().then(()=>{
     console.log("Connected to db");
 })
@@ -10,7 +15,7 @@ main().then(()=>{
     console.log(err)
 })
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 const initDB=  async ()=>{
     await Listing.deleteMany({});
